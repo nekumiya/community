6e1ce4218a541f64813c2e9e22670b65fbef2408
nekumiya <2383294608@qq.com>
2019-11-23 17:36:45 +0800

    init repo

---
+++ src/main/java/lemon/community/controller/HelloController.java
@@ -0,0 +1,18 @@
+package lemon.community.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name",required = true,defaultValue = "Nobody") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
